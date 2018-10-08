fc2974a4640fca8a26f08971ad55adefafe1702d
PE-Nom <ib65629@gmail.com>
2018-10-08 16:36:13 +0900

    初回登録のコミット

---
+++ src/components/Dashboard/Layout/DashboardLayout.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="wrapper">
+    <side-bar>
+      <mobile-menu slot="content"></mobile-menu>
+      <sidebar-link to="/admin/typography">
+        <i class="nc-icon nc-chart-pie-35"></i>
+        <p>イベント</p>
+      </sidebar-link>
+      <sidebar-link to="/admin/table-list">
+        <i class="nc-icon nc-circle-09"></i>
+        <p>指摘・良好 一覧</p>
+      </sidebar-link>
+      <sidebar-link to="/admin/table-list">
+        <i class="nc-icon nc-notes"></i>
+        <p>過去データ</p>
+      </sidebar-link>
+      <sidebar-link to="/admin/overview">
+        <i class="nc-icon nc-paper-2"></i>
+        <p>分析・解析</p>
+      </sidebar-link>
+      <sidebar-link to="/admin/user">
+        <i class="nc-icon nc-atom"></i>
+        <p>ユーザー管理</p>
+      </sidebar-link>
+      <sidebar-link to="/admin/notifications">
+        <i class="nc-icon nc-pin-3"></i>
+        <p>設定</p>
+      </sidebar-link>
+    </side-bar>
+    <div class="main-panel">
+      <top-navbar></top-navbar>
+
+      <dashboard-content @click="toggleSidebar">
+
+      </dashboard-content>
+
+      <content-footer></content-footer>
+    </div>
+  </div>
+</template>
+<style lang="scss">
+
+</style>
+<script>
+  import TopNavbar from './TopNavbar.vue'
+  import ContentFooter from './ContentFooter.vue'
+  import DashboardContent from './Content.vue'
+  import MobileMenu from './MobileMenu.vue'
+  export default {
+    components: {
+      TopNavbar,
+      ContentFooter,
+      DashboardContent,
+      MobileMenu
+    },
+    methods: {
+      toggleSidebar () {
+        if (this.$sidebar.showSidebar) {
+          this.$sidebar.displaySidebar(false)
+        }
+      }
+    }
+  }
+
+</script>
